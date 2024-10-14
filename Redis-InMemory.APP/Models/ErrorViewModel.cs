b765b9b3e1adbcec26af779654b63d360dbd065b
Deniz Doğru <deniz.dogru@venhancer.com>
2024-10-14 14:55:45 +0300

    Add project files.

---
+++ Redis-InMemory.APP/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Redis_InMemory.APP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
