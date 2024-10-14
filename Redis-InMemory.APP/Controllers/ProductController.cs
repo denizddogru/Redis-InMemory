d5287da56c5a10998f8692c7083c17d1e29c4fc9
Deniz Doğru <deniz.dogru@venhancer.com>
2024-10-14 15:25:25 +0300

    Chapter 2: Asp.Net Core + In-Memory -> We have created an MVC model and added DisplayTime and Index. Basically we cached the DateTime value, but it doesnt change yet because we havent defined a scope for it

---
+++ Redis-InMemory.APP/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Redis_InMemory.APP.Controllers;
+public class ProductController : Controller
+{
+
+    private readonly IMemoryCache _memoryCache;
+
+    public ProductController(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+    public IActionResult Index()
+    {
+
+        _memoryCache.Set<string>("Time", DateTime.Now.ToString());
+
+        return View();
+    }
+
+    public IActionResult DisplayTime()
+    {
+        ViewBag.Time = _memoryCache.Get<string>("Time");
+        return View();
+    }
+}
