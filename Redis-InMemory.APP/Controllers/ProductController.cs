e9a17ea230b530d7e83fcb84ea2d340e68908d2a
Deniz Doğru <deniz.dogru@venhancer.com>
2024-10-14 16:27:12 +0300

    AbsoluteExpiration, SlidingExpiration

--- Redis-InMemory.APP/Controllers/ProductController.cs
+++ Redis-InMemory.APP/Controllers/ProductController.cs
@@ -14,20 +14,31 @@ public class ProductController : Controller
     public IActionResult Index()
     {
 
-        // 1.yol
-        if (String.IsNullOrEmpty(_memoryCache.Get<string>("Time")))
-        {
-            _memoryCache.Set<string>("Time", DateTime.Now.ToString());
+        //// 1.yol
+        //if (String.IsNullOrEmpty(_memoryCache.Get<string>("Time")))
+        //{
+        //    _memoryCache.Set<string>("Time", DateTime.Now.ToString());
 
-        }
+        //}
 
         // 2.yol
 
-        if(!_memoryCache.TryGetValue("Time", out string timeCache))
-        {
-            _memoryCache.Set<string>("Time", DateTime.Now.ToString());
+        // Cachelenecek olan datayı set ettiğimiz kısım.
 
-        }
+
+        MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+
+        // 1 dakika süreyle, 4 kez cache'deki dataya erişlebilirz ( 4 çünkü sliding expiration 15 saniyeye set)
+        options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+        // Her 15 saniyede memory'yi yeniler. erişmez isek silinir.
+        options.SlidingExpiration = TimeSpan.FromSeconds(15);
+
+        // memory'de, Time oluştuğu zaman, 20 saniyelik oluşacak
+        _memoryCache.Set<string>("Time", DateTime.Now.ToString(), options);
+
+
 
 
         return View();
@@ -35,12 +46,38 @@ public class ProductController : Controller
 
     public IActionResult DisplayTime()
     {
-        _memoryCache.GetOrCreate<string>("Time", entry =>
-        {
-            return DateTime.Now.ToString();
-        });
+        //_memoryCache.GetOrCreate<string>("Time", entry =>
+        //{
+        //    return DateTime.Now.ToString();
+        //});
+
+        _memoryCache.TryGetValue("Time", out string timeCache);
+
+        ViewBag.Time = timeCache;
 
         ViewBag.Time = _memoryCache.Get<string>("Time");
         return View();
     }
+
+
+    // Not: İlk olarak https://localhost:7199/product/ 'e gidip index oluşturmak gerekiyor.
+    // ardından https://localhost:7199/product/displaytime 'a gidince 20 saniyelik süreyle cache'de tuttuğumuz datayı display ediyoruz.
+
 }
+
+/*
+ * Absolute Expiration (Mutlak Süre Sonu):
+ 
+Belirtilen kesin bir tarih veya süreden sonra önbellekteki veri otomatik olarak silinir.
+Örnek: "Bu veri 30 dakika sonra silinsin."
+
+
+Sliding Expiration (Kayan Süre Sonu):
+
+Veriye her erişildiğinde süre yenilenir.
+Belirtilen süre boyunca erişim olmazsa veri silinir.
+Örnek: "Son erişimden itibaren 10 dakika boyunca erişim olmazsa silinsin."
+
+
+/*
+
