ed695a0a0e12edbdb19da6ba4057cac5040e2415
Deniz Doğru <deniz.dogru@venhancer.com>
2024-10-19 20:47:56 +0300

    IDistributedCache-2 ( Caching of complex types ( string serialization,deserialization)

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace IDistributedCacheRedisApp.Web.Controllers;
 public class ProductsController : Controller
@@ -15,18 +17,29 @@ public class ProductsController : Controller
     {
         DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
 
-        cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+        cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(30);
 
-        await _distributedCache.SetStringAsync("surname","Dogru",  cacheEntryOptions);
-        await _distributedCache.SetStringAsync("isim", "Arzu", cacheEntryOptions);
+        //await _distributedCache.SetStringAsync("surname","Dogru",  cacheEntryOptions);
+        //await _distributedCache.SetStringAsync("isim", "Arzu", cacheEntryOptions);
+
+        Product product = new Product { Id = 2, Name = "Test2", Price = 105};
+        string jsonProduct = JsonConvert.SerializeObject(product);
+
+        await _distributedCache.SetStringAsync("product:2", jsonProduct, cacheEntryOptions);
+
+
 
         return View();
     }
 
     public IActionResult DisplayTime()
     {
-        string name = _distributedCache.GetString("isim");
+        //string name = _distributedCache.GetString("isim");
 
+        string jsonProduct = _distributedCache.GetString("product:1");
+        Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+        ViewBag.product = p;
         return View();
     }
 
