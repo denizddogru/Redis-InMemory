5078f85d12c10835750c272b6ce21e8241cb06c4
Deniz Doğru <deniz.dogru@venhancer.com>
2024-10-21 17:00:22 +0300

    Chapter 5: Asp.Net Core + StackExchange.Redis API, RedisService sınıfını oluşturduk. AnotherRedisDesktop üzerinden bir db seçip ona bağlanmak için config ayarlarını ekledik

---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+
+namespace IDistributedCacheRedisApp.Web.Services;
+
+public class RedisService
+{
+
+    private readonly string _redisHost;
+    private readonly string _redisPort;
+
+    // redis server ile haberleşeceğz.
+    private ConnectionMultiplexer _redis;
+
+    public IDatabase db { get; set; }
+    public RedisService(IConfiguration configuration)
+    {
+        _redisHost = configuration["Redis:Host"];
+        _redisPort = configuration["Redis:Port"];
+    }
+
+    public void Connect()
+    {
+        var configString = $"{_redisHost}:{_redisPort}";
+
+        _redis = ConnectionMultiplexer.Connect(configString);
+
+
+
+    }
+
+    // AnotherRedisDesktopManager üzerinden bir db seçeceğız.
+    public IDatabase GetDb(int db)
+    {
+        return _redis.GetDatabase(db);
+    }
+
+
+
+}
