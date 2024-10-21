d571f5242f1f782f0cb2027cb38e24da2c9f173b
Deniz Doğru <deniz.dogru@venhancer.com>
2024-10-21 17:24:41 +0300

    Redis.StackExchangeAPI / Redis String => We have added some string values to our redis db using StackExchange api

---
+++ RedisExchangeAPI.Web/Controllers/StringType.cs
@@ -0,0 +1,42 @@
+﻿using IDistributedCacheRedisApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers;
+public class StringType : Controller
+{
+    private readonly RedisService _redisService;
+    private readonly IDatabase db;
+    public StringType(RedisService redisService)
+    {
+        _redisService = redisService;
+        db = _redisService.GetDb(0);
+    }
+
+
+    // https://localhost:7010/stringtype
+    public IActionResult Index()
+    {
+        db.StringSet("name", "Deniz Dogru");
+        db.StringSet("visitor", 100);
+
+        return View();
+    }
+
+    // https://localhost:7010/stringtype/display
+
+    public IActionResult Display()
+    {
+
+        var redisValue = db.StringGet("name");
+
+        db.StringIncrement("visitor", 1);
+
+        if(redisValue.HasValue)
+        {
+            ViewBag.redisValue = redisValue.ToString();
+        }
+
+        return View();
+    }
+}
